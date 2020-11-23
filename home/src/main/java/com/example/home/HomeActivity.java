17955a0a45574d7adcd74a8fe0c2bde130b5bec5
Caixc <caixc@anchnet.com>
2020-11-23 16:40:01 +0800

    配置module AndroidManifest方便独立测试

--- home/src/main/java/com/example/home/HomeActivity.java
+++ home/src/main/java/com/example/home/HomeActivity.java
@@ -1,11 +1,7 @@
 package com.example.home;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.commonlib.BaseActivity;
 
 @Route(path = "/home/HomeActivity")
